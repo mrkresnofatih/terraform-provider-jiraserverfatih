cae267de9eed41223c8e076a0e739815e74fa669
mrkresnofatih <mrkresnofatih@gmail.com>
2023-03-11 17:02:45 +0700

    update tf provider

--- hashicups/Provider.go
+++ hashicups/Provider.go
@@ -13,7 +13,7 @@ func Provider() *schema.Provider {
 	return &schema.Provider{
 		DataSourcesMap: map[string]*schema.Resource{},
 		ResourcesMap: map[string]*schema.Resource{
-			"jiraserverfatih_projectrole_resource": resources.ProjectRoleResource(),
+			"jiraserverfatih_projectroleresource": resources.ProjectRoleResource(),
 		},
 		Schema: map[string]*schema.Schema{
 			"domain": &schema.Schema{
