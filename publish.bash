b6610c9f6b40787b27d0b823ae1c0649ac49a893
mrkresnofatih <mrkresnofatih@gmail.com>
2023-03-12 22:40:19 +0700

    update tf provider to version v1.0.20230312224019-dev

--- publish.bash
+++ publish.bash
@@ -1,4 +1,4 @@
 #! /bin/bash
 VersionId=`date +%Y%m%d%H%M%S`
 touch release.txt && echo "v1.0."$VersionId"-dev" > release.txt
-git add . && git status && git commit -m "update tf provider to version v1.0."$VersionId"-dev" && git push origin dev && git tag "v1.0."$VersionId"-dev" && git push origin "v1.0."$VersionId"-dev"
+git add . && git status && git commit -m "update tf provider to version v1.0."$VersionId"-dev" && git tag "v1.0."$VersionId"-dev" && git push origin "v1.0."$VersionId"-dev"
