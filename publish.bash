46b186e4ec536eb2b0ddaab7437a3335f7493bf8
mrkresnofatih <mrkresnofatih@gmail.com>
2023-03-12 20:48:34 +0700

    update bash script

--- publish.bash
+++ publish.bash
@@ -1,3 +1,3 @@
 #! /bin/bash
 VersionId=`date +%Y%m%d%H%M%S`
-git add . && git status && git commit -m "update tf provider" && git push origin dev && git tag "v1.0."$VersionId"-dev" && git push origin "v1.0."$VersionId"-dev"
+git add . && git status && git commit -m "update tf provider to version v"$VersionId"-dev" && git push origin dev && git tag "v1.0."$VersionId"-dev" && git push origin "v1.0."$VersionId"-dev"
